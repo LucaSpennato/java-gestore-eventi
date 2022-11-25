cc71b821c543b3db91830b76a20bb7f53ab82f70
Luca Spennato <luca.spennato@gmail.com>
2022-11-25 09:59:10 +0100

    testing in main

--- src/main/java/org/generation/italy/eventi/Evento.java
+++ src/main/java/org/generation/italy/eventi/Evento.java
@@ -1,7 +1,58 @@
 package org.generation.italy.eventi;
 
+import java.time.LocalDate;
+
 public class Evento {
 
+	private String title;
+	private LocalDate date;
+	private int totalSeats;
+	private int bookedSeats;
 	
+	public Evento(String title, LocalDate date, int totalSeats, int bookedSeats) {
+		
+		setTitle(title);
+		setDate(date);
+		setTotalSeats(totalSeats);
+		setBookedSeats(bookedSeats);
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	public int getTotalSeats() {
+		return totalSeats;
+	}
+	private void setTotalSeats(int totalSeats) {
+		this.totalSeats = totalSeats;
+	}
+	
+	public int getBookedSeats() {
+		return bookedSeats;
+	}
+	private void setBookedSeats(int bookedSeats) {
+		this.bookedSeats = bookedSeats;
+	}
+	
+	
+	@Override
+	public String toString() {
+
+		return "Title: " + getTitle()
+				+ "\nDate: " + getDate()
+				+ "\nTotal seats: " + getTotalSeats()
+				+ "\nBooked seats: " + getBookedSeats();
+	}
 	
 }
--- src/main/java/org/generation/italy/eventi/Main.java
+++ src/main/java/org/generation/italy/eventi/Main.java
@@ -1,9 +1,20 @@
 package org.generation.italy.eventi;
 
+import java.time.LocalDate;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		
+		Evento ev = new Evento("sagra della polpetta", LocalDate.now(), 50, 20);
+		
+		System.out.println(ev);
+		
+		System.out.println("--------------------------");
+		ev.setTitle("Sagra della polpetta di castrignano del capo provincia di belluno");
+		ev.setDate(LocalDate.parse("3000-12-13"));
+		System.out.println(ev);
+
 	}
 	
 }
